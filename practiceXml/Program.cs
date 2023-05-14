e7f8a1a16d2092e97126af6273d3a6c36c3cabb3
Aleks <Aleks@ASER>
2023-05-14 11:47:59 +0500

    Добавьте файлы проекта.

---
+++ practiceXml/Program.cs
@@ -0,0 +1,57 @@
+﻿using practiceXml;
+using System.Xml;
+
+XmlDocument doc = new XmlDocument();
+try
+{
+    
+    doc.LoadXml("<user>" + "<user1>" + "<Id>1</Id>" + "<Name>user1</Name>" + "<Email>a@gmail.com</Email>" + "</user1>" +
+        "<user2>" + "<Id>2</Id>" + "<Name>user2</Name>" + "<Email>b@gmail.com</Email>" + "</user2>"+
+        "<user3>" + "<Id>3</Id>" + "<Name>user3</Name>" + "<Email>c@gmail.com</Email>" + "</user3>"+ "</user>");
+    doc.Save("file.xml");
+
+    Console.WriteLine("Файл сохранен\n");
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка c cохр: {ex.Message}");
+}
+
+
+
+XmlDocument xmlDocument = new XmlDocument();
+xmlDocument.Load("file1.xml");
+
+List<User> users = new List<User>();
+XmlElement xmlMainElement = xmlDocument.DocumentElement;
+foreach(XmlElement xmlElement in xmlMainElement)
+{
+    User user = new User();
+    foreach(XmlNode xmlNode in xmlElement.ChildNodes)
+    {
+        if(xmlNode.Name == "Id")
+        {
+            user.Id =int.Parse(xmlNode.InnerText);
+            Console.WriteLine($"Id: {xmlNode.InnerText}");
+        }
+        if(xmlNode.Name == "Name")
+        {
+            user.Name = xmlNode.InnerText;
+            Console.WriteLine($"Name: {xmlNode.InnerText}");
+        }
+        if(xmlNode.Name == "Email")
+        {
+            user.Email = xmlNode.InnerText;
+            Console.WriteLine($"Email: {xmlNode.InnerText}");
+        }
+    }
+
+    users.Add(user);
+}
+
+
+//foreach (var i in users)
+//{
+//    Console.WriteLine($"{i.Id} {i.Name} {i.Email}\n");
+//}
---
+++ practiceXml_Json/Program.cs
@@ -0,0 +1,57 @@
+﻿using practiceJson;
+using System.Text.Json;
+
+//User user1 = new(1, "Aleks", "a@gmail.com");
+
+List<User> users = new List<User>
+{
+    new(1, "user1", "a@gmail.com"),
+    new(2, "user2", "b@gmail.com"),
+    new(3, "user3", "c@gmail.com"),
+    new(4, "user4", "d@gmail.com")
+};
+var options = new JsonSerializerOptions
+{
+    WriteIndented = true
+};
+string json = JsonSerializer.Serialize(users, options);
+Console.WriteLine($"\nИз класса в json: \n {json}");
+
+
+List<User> unjson = JsonSerializer.Deserialize<List<User>>(json);
+Console.WriteLine("\nИз json в класс: ");
+foreach (User user in unjson)
+{
+    Console.WriteLine($"\n {user.Id} \n {user.Name} \n {user.Email}");
+}
+
+
+try
+{
+    string file = "user.json";
+    File.WriteAllText(file, json);
+    Console.WriteLine("\nФайл сохранен");
+
+
+    Console.WriteLine("\nЧтение из файла: ");
+    string filejson = File.ReadAllText(file);
+    List<User> jsonstring = JsonSerializer.Deserialize<List<User>>(filejson);
+    foreach (User user in jsonstring)
+    {
+        Console.WriteLine($"\n{user.Id} \n {user.Name} \n {user.Email}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка с сохр: {ex.Message}");
+}
+
+
+
+
+
+
+
+
+
+
